b11a424307340d6ac80f961f97c3d39533cc2e68
hardipparmar <hardip.multidots@gmail.com>
2017-12-19 17:02:02 +0530

    updated

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -18,6 +18,22 @@
  * @package WordPress
  */
 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
